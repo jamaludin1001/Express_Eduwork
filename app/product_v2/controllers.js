0de2055e8d0ed1d0c365108631623b1ab7cef432
Jamaludin Saputra <jamaludinsaputraaa@gmail.com>
2022-12-09 20:28:48 +0700

    update node module

--- app/product_v2/controllers.js
+++ app/product_v2/controllers.js
@@ -1,107 +1,125 @@
-const { Op } = require('sequelize');
-const Product = require('./model');
-const fs = require('fs');
-const path = require('path');
+const { Op } = require("sequelize");
+const Product = require("./model");
+const fs = require("fs");
+const path = require("path");
 
-const store = async(req, res) =>{
-    const {users_id, name, price, stock, status} = req.body;
-    const image = req.file;
-    
-    if (image){
-        const target = path.join(__dirname,'../../uploads',image.originalname);
-        fs.renameSync(image.path,target);
-    }
-    try {
-        await Product.sync();
-        await Product.create({users_id, name, price, stock, status, image_url: `http://localhost:3000/public/${image.originalname}`});
-        res.json({
-            "message": "Product Created"
-        });
-    } catch(e) {
-        res.send(e);
-    }
-}
+const store = async (req, res) => {
+  const { users_id, name, price, stock, status } = req.body;
+  const image = req.file;
 
-const view = async(req, res) =>{
-    try {
-        const product = await Product.findAll({
-            where: {
-                id: req.params.id
-              }
-        });
-        res.send(product[0]);
-    } catch (err) {
-        console.log(err);
-    }
-}
+  if (image) {
+    const target = path.join(__dirname, "../../uploads", image.originalname);
+    fs.renameSync(image.path, target);
+  }
+  try {
+    await Product.sync();
+    await Product.create({
+      users_id,
+      name,
+      price,
+      stock,
+      status,
+      image_url: `http://localhost:3000/public/${image.originalname}`,
+    });
+    res.json({
+      message: "Product Created",
+    });
+  } catch (e) {
+    res.send(e);
+  }
+};
 
-const index = async(req, res) =>{
-    const {search} = req.query;
-    let exec = {};
-    if (search){
-        exec = {
-            where:{
-                name:{
-                    [Op.like]: `%${search}%`
-                }
-            }
-        }
-    }
- 
-    try {
-        const product = await Product.findAll(exec);
-        res.send(product);
-    } catch (err) {
-        console.log(err);
-    }
-}
+const view = async (req, res) => {
+  try {
+    const product = await Product.findAll({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.send(product[0]);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-const update = async(req, res) =>{
-    const {users_id, nama_produk, harga, stok, status} = req.body;
-    const image = req.file;
-    let dataQuery = {};
-    
-    if (image){
-        const target = path.join(__dirname,'../../uploads',image_url.originalname);
-        fs.renameSync(image_url.path,target);
-        dataQuery = {users_id, nama_produk, harga, stok, status, image_url: `http://localhost:5000/public/${image_url.originalname}`}
-    } else {
-        dataQuery = {users_id, nama_produk, harga, stok, status}
-    }
-    try {
-        await Product.sync();
-        await Product.update(dataQuery, {
-            where: {
-                id: req.params.id
-            }
-        });
-        res.json({
-            "message": "Product Updated"
-        });
-    } catch(e) {
-        res.send(e);
-    }
-}
+const index = async (req, res) => {
+  const { search } = req.query;
+  let exec = {};
+  if (search) {
+    exec = {
+      where: {
+        name: {
+          [Op.like]: `%${search}%`,
+        },
+      },
+    };
+  }
 
-const destruct = async(req, res) =>{
-    try {
-         await Product.destroy({
-            where: {
-                id: req.params.id
-              }
-        });
-        res.json({
-            "message": "Product Deleted"
-        });
-    } catch (err) {
-        console.log(err);
-    }
-}
+  try {
+    const product = await Product.findAll(exec);
+    res.send(product);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const update = async (req, res) => {
+  const { users_id, nama_produk, harga, stok, status } = req.body;
+  const image = req.file;
+  let dataQuery = {};
+
+  if (image) {
+    const target = path.join(
+      __dirname,
+      "../../uploads",
+      image_url.originalname
+    );
+    fs.renameSync(image_url.path, target);
+    dataQuery = {
+      users_id,
+      nama_produk,
+      harga,
+      stok,
+      status,
+      image_url: `http://localhost:5000/public/${image_url.originalname}`,
+    };
+  } else {
+    dataQuery = { users_id, nama_produk, harga, stok, status };
+  }
+  try {
+    await Product.sync();
+    await Product.update(dataQuery, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json({
+      message: "Product Updated",
+    });
+  } catch (e) {
+    res.send(e);
+  }
+};
+
+const destruct = async (req, res) => {
+  try {
+    await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json({
+      message: "Product Deleted",
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 module.exports = {
-    store,
-    view,
-    index,
-    update,
-    destruct
-}
+  store,
+  view,
+  index,
+  update,
+  destruct,
+};
